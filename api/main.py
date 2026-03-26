b0c5bea6b5dad10babae19c92e5927b749222e6c
OlivierArthur <163600465+OlivierArthur@users.noreply.github.com>
2026-03-26 23:21:30 +0100

    Dodanie kodu

---
+++ api/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+
+app = FastAPI(title="Klasyfikator SPAM AP  I")
+
+NAZWA = "OliverArt5500/klasyfikatorspamu1"
+
+klasyfikator = pipeline("text-classification", model=NAZWA, tokenizer=NAZWA)
+
+class Email(BaseModel):
+    text: str
+
+@app.post("/predict")
+def predict(email: Email):
+    wyniki = klasyfikator(email.text, truncation=True, max_length=512)
+    return {"prediction": wyniki[0]}
